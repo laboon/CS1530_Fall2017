8d4a4320b1623bf01af6457df269c46f6918bd7c
laboon <laboon@gmail.com>
2017-10-04 12:21:21 -0400

    add link

--- sample_code/unsafe/SoUnsafe.java
+++ sample_code/unsafe/SoUnsafe.java
@@ -1,6 +1,13 @@
 import sun.misc.*;
 import java.lang.reflect.*;
 
+// For more fun things you can do with unsafe, such as modify
+// private variables, throw checked exceptions even if they are not
+// checked for, store information off the heap, write data
+// wherever you want (the 10th element of a 4-element array?  SURE!),
+// allocate native memory, etc. check out:
+// http://www.baeldung.com/java-unsafe
+
 public class SoUnsafe {
     
     public static Unsafe getUnsafe() {
